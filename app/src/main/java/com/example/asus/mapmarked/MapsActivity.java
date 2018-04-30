8c074d396473d4f27c53c6baa67870f7a8749e78
Istiak <sifatistiak@gmail.com>
2018-05-01 04:25:28 +0600

    final

--- app/src/main/java/com/example/asus/mapmarked/MapsActivity.java
+++ app/src/main/java/com/example/asus/mapmarked/MapsActivity.java
@@ -2,18 +2,28 @@ package com.example.asus.mapmarked;
 
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
-
+public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback {
+    Double Lng,Lat;
     private GoogleMap mMap;
-
+    private DatabaseReference mDatabaseReference;
+    private DatabaseReference mDatabaseReference1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,24 +33,80 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
     }
+    
 
-
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     @Override
-    public void onMapReady(GoogleMap googleMap) {
+    public void onMapReady(final GoogleMap googleMap) {
         mMap = googleMap;
+        mDatabaseReference = FirebaseDatabase.getInstance().getReference().child("Location/longitude");
+        mDatabaseReference1 = FirebaseDatabase.getInstance().getReference().child("Location/latitude");
 
-        // Add a marker in Sydney and move the camera
-        LatLng newaddress = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(newaddress).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(newaddress));
+
+        mDatabaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                mMap.clear();
+
+                final Double Lng = dataSnapshot.getValue(Double.class);
+                mDatabaseReference1.addValueEventListener(new ValueEventListener() {
+
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        Double Lat = dataSnapshot.getValue(Double.class);
+                        LatLng newaddress = new LatLng(Lat,Lng);
+
+                        mMap.addMarker(new MarkerOptions().position(newaddress).title(newaddress.toString()).icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN)));
+                        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(newaddress,19));
+                        mMap.setMinZoomPreference(6.0f);
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+
+                    }
+                });
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                Toast.makeText(getApplicationContext(), "Error",
+                        Toast.LENGTH_LONG).show();
+
+            }
+
+        });
+        mDatabaseReference1.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                mMap.clear();
+
+                final Double Lng = dataSnapshot.getValue(Double.class);
+                mDatabaseReference.addValueEventListener(new ValueEventListener() {
+
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        Double Lat = dataSnapshot.getValue(Double.class);
+                        LatLng newaddress = new LatLng(Lat,Lng);
+
+                        mMap.addMarker(new MarkerOptions().position(newaddress).title(newaddress.toString()).icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_BLUE)));
+                        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(newaddress,19));
+                        mMap.setMinZoomPreference(6.0f);
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+
+                    }
+                });
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                Toast.makeText(getApplicationContext(), "Error",
+                        Toast.LENGTH_LONG).show();
+
+            }
+
+        });
     }
 }
